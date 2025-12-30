5eb338a1e008c5bf58311ca2cec03ec7ea015e0a
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2025-12-30 21:46:08 +0530

    created simple routing

--- app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View className="flex-1 bg-blue-400 justify-center items-center">
-      <Text className="text-blue-500 font-semibold">Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
